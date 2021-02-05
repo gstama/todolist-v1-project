40163e786137901d55057fb657fe7f45fd4159ea
gstama <gstama@outlook.com>
2021-02-05 16:24:14 +0000

    css + google.fonts + css classes in list.ejs

--- app.js
+++ app.js
@@ -3,19 +3,20 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 var today = new Date();
-var items = [];
+const items = [];
 
 app.get('/', (req, res, next) => {
-  var options = {
+  let options = {
     weekday: 'long',
     day: 'numeric',
     month: 'long',
   };
 
-  var day = today.toLocaleDateString('en-US', options);
+  let day = today.toLocaleDateString('en-US', options);
 
   res.render('list', {
     day: day,
