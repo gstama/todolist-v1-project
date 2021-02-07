e0d5df62e03b62386273ab9556186104e87ee1a2
gstama <gstama@outlook.com>
2021-02-07 16:30:27 +0000

    adding MongoDB integration with mongoose

--- app.js
+++ app.js
@@ -1,20 +1,59 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const date = require(__dirname + '/util/date.js');
+const config = require(__dirname + '/config.js');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-const items = [];
-const workItems = [];
+mongoose.connect(config.mongoConnectURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const itemsSchema = new mongoose.Schema({
+  name: String,
+});
+
+const Item = mongoose.model('Item', itemsSchema);
 
 app.get('/', (req, res, next) => {
   let day = date.getDate();
-  res.render('list', {
-    listTitle: day,
-    items: items,
+
+  Item.find({}, (err, items) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (items.length === 0) {
+        const defaultItem1 = new Item({
+          name: 'Welcome to your todo list!',
+        });
+        const defaultItem2 = new Item({
+          name: 'Hit the + button to add a new item.',
+        });
+        const defaultItem3 = new Item({
+          name: '<-- Hit this to delete an item.',
+        });
+
+        const defaultItems = [defaultItem1, defaultItem2, defaultItem3];
+
+        Item.insertMany(defaultItems, (err) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log('Successfully saved default items to database');
+          }
+        });
+        res.redirect('/');
+      }
+      res.render('list', {
+        listTitle: day,
+        items: items,
+      });
+    }
   });
 });
 
@@ -23,7 +62,16 @@ app.post('/', (req, res, next) => {
     workItems.push(req.body.newItem);
     res.redirect('/work');
   } else {
-    items.push(req.body.newItem);
+    const item = new Item({
+      name: req.body.newItem,
+    });
+    item.save((err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Successfully added new item.');
+      }
+    });
     res.redirect('/');
   }
 });
